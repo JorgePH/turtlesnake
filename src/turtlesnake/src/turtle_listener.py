b339e398a4bc59cf3ce51d9247aa1129920d66fb
JorgePH <j.povedah@alumnos.upm.es>
2020-09-24 11:53:37 +0200

    Changed to class implementation. Implemented Empty start_turtlesim_snake service

--- src/turtlesnake/src/turtle_listener.py
+++ src/turtlesnake/src/turtle_listener.py
@@ -1,44 +1,74 @@
 #!/usr/bin/env python  
-import roslib
-roslib.load_manifest('turtlesnake')
+
 import rospy
 import math
 import tf
 import geometry_msgs.msg
 import turtlesim.srv
+from std_srvs.srv import Empty
+
+
+class TurtleListenerNode:
+	# Square distance to catch the new turtle
+	following_distance = 0.5
+
+	def __init__(self):
+		rospy.init_node('turtle_tf_listener')
+		rospy.loginfo("Starting turtle listener node.")
+
+		# Flag to know if the other turtle is following
+		self.following = False
+		# Whether the snake game has started or not
+		self.turtlesim_snake = False
+
+		self.listener = tf.TransformListener()
+
+		# Advertise the service start turtlesim
+		rospy.Service('/start_turtlesim_snake', Empty, self.start_turtlesim_snake)
+
+		rospy.loginfo("Spawning new turtle.")
+		rospy.wait_for_service('spawn')
+		spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+		spawner(4, 2, 0, 'turtle2')
+
+		self.turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
+
+		self.rate = rospy.Rate(10)
+
+	def run(self):
+		while not rospy.is_shutdown():
+			# Get transform
+			try:
+				(trans, rot) = self.listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+			except (tf.Exception, tf.LookupException, tf.ConnectivityException):
+				continue
+
+			if self.turtlesim_snake is True:
+				if self.following is False:
+					if math.sqrt(trans[0] ** 2 + trans[1] ** 2) < self.following_distance:
+						rospy.loginfo("Now turtle follows.")
+						self.following = True
+
+				if self.following is True:
+					angular = 4 * math.atan2(trans[1], trans[0])
+					linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+					cmd = geometry_msgs.msg.Twist()
+					cmd.linear.x = linear
+					cmd.angular.z = angular
+					self.turtle_vel.publish(cmd)
+
+			self.rate.sleep()
+
+	def start_turtlesim_snake(self, req):
+		rospy.loginfo("Starting turtlesim snake.")
+		self.turtlesim_snake = True
+		return []
+
+
+def main():
+	turtle_listener = TurtleListenerNode()
+	turtle_listener.run()
+
 
 if __name__ == '__main__':
-	rospy.init_node('turtle_tf_listener')
-
-	following_distance = 0.5
-	following = False
-
-	listener = tf.TransformListener()
-
-	rospy.wait_for_service('spawn')
-	spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-	spawner(4, 2, 0, 'turtle2')
-
-	turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
-
-	rate = rospy.Rate(10.0)
-	while not rospy.is_shutdown():
-		# Get transform
-		try:
-			(trans,rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
-		except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-			continue
-				
-		if following is False:
-			if math.sqrt(trans[0] ** 2 + trans[1] ** 2) < following_distance:
-				following = True
-				
-		if following is True:				
-			angular = 4 * math.atan2(trans[1], trans[0])
-			linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-			cmd = geometry_msgs.msg.Twist()
-			cmd.linear.x = linear
-			cmd.angular.z = angular
-			turtle_vel.publish(cmd)
-
-		rate.sleep()
+	main()
