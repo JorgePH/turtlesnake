acf0a931614bbc178cc97ec4239fc1723310d3fe
JorgePH <j.povedah@alumnos.upm.es>
2020-09-24 12:36:14 +0200

    Moved turtle spawning into the service call

--- src/turtlesnake/src/turtle_listener.py
+++ src/turtlesnake/src/turtle_listener.py
@@ -16,34 +16,28 @@ class TurtleListenerNode:
 		rospy.init_node('turtle_tf_listener')
 		rospy.loginfo("Starting turtle listener node.")
 
+		# Advertise the service start turtlesim
+		rospy.Service('/start_turtlesim_snake', Empty, self.start_turtlesim_snake)
+
+		# Init some variables for the second turtle
 		# Flag to know if the other turtle is following
 		self.following = False
 		# Whether the snake game has started or not
 		self.turtlesim_snake = False
-
-		self.listener = tf.TransformListener()
-
-		# Advertise the service start turtlesim
-		rospy.Service('/start_turtlesim_snake', Empty, self.start_turtlesim_snake)
-
-		rospy.loginfo("Spawning new turtle.")
-		rospy.wait_for_service('spawn')
-		spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-		spawner(4, 2, 0, 'turtle2')
-
-		self.turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
-
+		self.listener = None
+		self.turtle_vel = None
 		self.rate = rospy.Rate(10)
+
 
 	def run(self):
 		while not rospy.is_shutdown():
-			# Get transform
-			try:
-				(trans, rot) = self.listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
-			except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-				continue
-
 			if self.turtlesim_snake is True:
+				# Get transform
+				try:
+					(trans, rot) = self.listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+				except (tf.Exception, tf.LookupException, tf.ConnectivityException):
+					continue
+
 				if self.following is False:
 					if math.sqrt(trans[0] ** 2 + trans[1] ** 2) < self.following_distance:
 						rospy.loginfo("Now turtle follows.")
@@ -61,6 +55,16 @@ class TurtleListenerNode:
 
 	def start_turtlesim_snake(self, req):
 		rospy.loginfo("Starting turtlesim snake.")
+
+		self.listener = tf.TransformListener()
+
+		rospy.loginfo("Spawning new turtle.")
+		rospy.wait_for_service('spawn')
+		spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+		spawner(4, 2, 0, 'turtle2')
+
+		self.turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
+
 		self.turtlesim_snake = True
 		return []
 
